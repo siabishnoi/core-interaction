80ea3799e4d3f2cc8c199575820b15ada61511eb
siabishnoi <bishs960@newschool.edu>
2020-04-13 16:52:55 +0530

    new file

---
+++ SessionK-Studio/Week11-Class02/script.js
@@ -0,0 +1,21 @@
+var breakPoint = 768;
+
+var boxes = document.querySelectorAll('.box');
+
+if (window.innerWidth < breakPoint) {
+  window.addEventListener('scroll', () => {
+    boxes.forEach(box => {
+      var showItemAt = window.innerHeight + window.scrollY - box.offsetHeight / 2;
+      if (showItemAt > box.offsetTop) {
+        box.classList.add('appear');
+      } else {
+        box.classList.remove('appear');
+      }
+    });
+  });
+} else {
+  console.log('big screen');
+  boxes.forEach(box => {
+    box.classList.add('fade-in');
+  });
+}
