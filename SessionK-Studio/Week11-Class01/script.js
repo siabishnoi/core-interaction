07e7f94309d99ef858a52e9ec0774e27b7b1cb35
siabishnoi <bishs960@newschool.edu>
2020-04-20 22:59:49 +0530

    newfile

--- SessionK-Studio/Week11-Class01/script.js
+++ SessionK-Studio/Week11-Class01/script.js
@@ -1,5 +1,6 @@
 document.getElementById("img1").onclick=function(){
 	document.getElementById("ray").style.color='#33cc33';
+	
 }
 document.getElementById("img3").onclick=function(){
 	document.getElementById("francene").style.color='#33cc33';
@@ -9,4 +10,17 @@ document.getElementById("img2").onclick=function(){
 }
 document.getElementById("img4").onclick=function(){
 	document.getElementById("weird").style.color='red';
+}
+document.getElementById("img5").onclick=function(){
+	document.getElementById("gun").style.color='red';
+	
+}
+document.getElementById("img6").onclick=function(){
+	document.getElementById("knife").style.color='#33cc33';
+}
+document.getElementById("img7").onclick=function(){
+	document.getElementById("choke").style.color='#33cc33';
+}
+document.getElementById("img8").onclick=function(){
+	document.getElementById("rope").style.color='red';
 }
